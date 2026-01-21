a67eb0724f6bf13972a9459123ab3b8c86424443
RomeoRoshanGit <romeoroshan2024@gmail.com>
2026-01-21 23:35:29 +0530

    feat: Json to Interface (#4)

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,5 +1,8 @@
 import redactSecretsIcon from "@/assets/icons/redactsecrets.png";
+import jsonToInterfaceIcon from "@/assets/icons/json-to-interface.png";
 
 export const appIcons = {
-    redactSecretsIcon
+    redactSecretsIcon,
+    jsonToInterfaceIcon
 };
+
