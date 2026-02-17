4d779a5c6d63c4af81b499cdef239c47f4739a4e
Don Benny <donbenny@zohomail.in>
2026-02-17 20:53:25 +0530

    Revert "Feat/bgremover"

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-        ],
-      },
-      {
-        source: "/imgly/(.*)",
-        headers: [
-          {
-            key: "Cross-Origin-Resource-Policy",
-            value: "cross-origin",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
