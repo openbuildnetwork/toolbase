01200c358a7237ade7a0fdbfe369bb9f882b34b5
donC20 <donbenny@zohomail.in>
2026-05-20 01:08:50 +0530

    fix : remove tf files

--- infra/modules/cdn/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_domain_name" {
-  description = "The default CloudFront distribution domain name"
-  value       = aws_cloudfront_distribution.cdn.domain_name
-}
-
-output "cloudfront_distribution_id" {
-  description = "The CloudFront distribution ID"
-  value       = aws_cloudfront_distribution.cdn.id
-}
